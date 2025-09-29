697c9b476f050a9d2136ffbc29d9f50b9db90c53
vanajachowdary16 <vanajachowdary12@gmail.com>
2025-09-29 16:14:22 +0530

    orange hrm site automation

---
+++ src/test/java/Hrm/testpages/PIMPageObjects.java
@@ -0,0 +1,10 @@
+package Hrm.testpages;
+
+import org.openqa.selenium.WebDriver;
+
+import Hrm.testutils.HRMBaseTest;
+
+public class PIMPageObjects extends HRMBaseTest{
+	
+
+}
--- src/test/java/Hrm/testutils/HRMBaseTest.java
+++ src/test/java/Hrm/testutils/HRMBaseTest.java
@@ -4,23 +4,27 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class HRMBaseTest {
 	
-	WebDriver driver;
-	@BeforeClass
-	public void setUp() {
-	    WebDriverManager.chromedriver().setup(); // auto-manages driver
-	    driver = new ChromeDriver();
+	public static WebDriver driver;
+	
+	public static void LaunchBrowser() {
+		ChromeOptions chromeoptions = new ChromeOptions();
+		chromeoptions.addArguments("--disable-notifications");
+	    WebDriverManager.chromedriver().setup(); 
+	    driver = new ChromeDriver(chromeoptions);
 	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	    driver.manage().window().maximize();
 	    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 	}
-	@AfterClass
+	public static WebDriver getDriver() {
+		return driver;
+	}
+	
 	public void tearDown() {
 		driver.quit();
 	}
